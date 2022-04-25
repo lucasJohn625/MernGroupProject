9e61bc88f20577423d3d60330d55e18e4d72322a
brett1185 <89299506+brett1185@users.noreply.github.com>
2022-04-24 21:52:47 -0400

    server for blogs

---
+++ server/models/blog.model.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+
+const BlogSchema=new mongoose.Schema({
+    blogAuthor:{
+        type:String,
+        minLength:[5,"Must enter your name!"]
+    },
+    blogTitle:{
+        type:String,
+        required:[true,"Must enter a title for the blog!"]
+    },
+    text:{
+        type:String,
+        minLength:[3, "Blog must be between 3 and 200 characters"],
+        maxLength:[200, "Blog must be between 3 and 200 characters"]
+
+    },
+    date:{
+        type:Date,
+        required:[true, "Please enter a date"]
+    }
+},{timestamps:true})
+
+const Blog =mongoose.model('Blog', BlogSchema)
+
+module.exports =Blog
